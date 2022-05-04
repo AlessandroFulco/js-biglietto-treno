c0f831f5dcca923932c33c91cc564b982c15de09
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-05-04 15:52:40 +0200

    calcolo sconto ticket

--- js/script.js
+++ js/script.js
@@ -15,9 +15,27 @@ const pricePerKm = 0.21;
 console.log("prezzo al km", pricePerKm);
 const priceTicket = chilometri * pricePerKm;
 console.log(priceTicket.toFixed(2) + " €");
-// se minorenne applicare 20% di sconto
 
-// se over 65 applicare il 40% di sconto
+// se minorenne applicare 20% di sconto
+let sale20;
+let sale40;
+let price20Off;
+let price40Off;
+if (age < 18) {
+    sale20 = (priceTicket / 100) * 20;
+    console.log("sconto da applicare 20%", sale20);
+    price20Off = priceTicket - sale20;
+    console.log(price20Off.toFixed(2) + " €");
+} else if (age >= 65) {
+    // se over 65 applicare il 40% di sconto
+    sale40 = (priceTicket / 100) * 40;
+    console.log("sconto da applicare 40%", sale40);
+    price40Off = priceTicket - sale40;
+    console.log(price40Off.toFixed(2) + " €");
+}
+
+
+
 
 // output stampare il risultato a schermo
 
