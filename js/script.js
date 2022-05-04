f8a82827f28e940a6753b120040601724d971507
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-05-04 15:12:42 +0200

    macro script

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// creare un programma che dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
+
+
+// chiedere il età al passeggero
+
+// chiedere quanti chilometri deve percorrere
+
+// calcolare il prezzo del biglietto (0,21€/km)
+
+// se minorenne applicare 20% di sconto
+
+// se over 65 applicare il 40% di sconto
+
+// output stampare il risultato a schermo
