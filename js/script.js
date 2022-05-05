f41a36bb7d488418bc3df4807e0b0025cbda6487
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-05-05 10:03:55 +0200

    refactory ottimizzato

--- js/script.js
+++ js/script.js
@@ -11,28 +11,25 @@ const age = parseInt(prompt('Quanti anni hai ?'));
 console.log("age", age);
 
 
-// se minorenne applicare 20% di sconto
-const prezzoMinorenne = 0.21 * 80 / 100;
-console.log(prezzoMinorenne);
 
-const prezzo65over = 0.21 * 60 / 100;
-console.log(prezzo65over);
-
+// calcolare prezzo in base all'età
+const standardPrice = 0.21;
 let priceTicket;
 
 if (age < 18) {
+    const prezzoMinorenne = standardPrice * 80 / 100;
     priceTicket = chilometri * prezzoMinorenne;
     console.log(priceTicket.toFixed(2));    
 } else if (age >= 65) {
+    const prezzo65over = standardPrice * 60 / 100;
     priceTicket = chilometri * prezzo65over;
     console.log(priceTicket.toFixed(2)); 
 } else {
-    priceTicket = chilometri * 0.21;
+    priceTicket = chilometri * standardPrice;
     console.log(priceTicket.toFixed(2));
 }
 
-document.getElementById('ticket').innerHTML = "Il costo del Biglietto è: " + priceTicket.toFixed(2) + " €";
-
 // output stampare il risultato a schermo
+document.getElementById('ticket').innerHTML = "Il costo del Biglietto è: " + priceTicket.toFixed(2) + " €";
 
 
